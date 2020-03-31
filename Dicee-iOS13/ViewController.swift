39079e2708e4e555de9c87e57baee97ca835b199
Osaro Iyoha <saro.iyoha@gmail.com>
2020-03-31 17:49:57 +0100

    Increment Number on Roll button click

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,26 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
     //viewDidLoad() func handles what happens when the app loads up
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        // Who.what = value;
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        
-        //Use alpha to set Opacity
-        diceImageView1.alpha = 0.5
-        
-        diceImageView2.image = #imageLiteral (resourceName: "DiceThree")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //print("Button Got Tapped");
+        //Use alpha to set Opacity
+//        diceImageView1.alpha = 1
+        // Who.what = value;
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        //rightDiceNumber Condition
+       
+        //leftDiceNumber Condition
+        if (leftDiceNumber<=4) {
+            leftDiceNumber = leftDiceNumber + 1;
+        } else {
+            leftDiceNumber = 0
+        }
         
-        diceImageView1.alpha = 1
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        if (rightDiceNumber>0) {
+            rightDiceNumber = rightDiceNumber - 1;
+        } else {
+            rightDiceNumber = 5
+        }
+        
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
         
     }
     
